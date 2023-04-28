04e39824185cec1a2e2a5ee9b97baf6c3843e374
Lucas Alves <zombnfl@gmail.com>
2023-04-28 15:57:38 -0300

    Implementação de políticas de CORS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", '*');
+    res.header("Access-Control-Allow-Methods", '*');
+    app.use(cors());
+    next();
+})
+
 
 const sequelize = require("./db");
 
